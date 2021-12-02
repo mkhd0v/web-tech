69ab257a24377ab3759e17a248c5d5c626322486
00009448 <wiut9448@gmail.com>
2021-12-02 22:51:25 +0500

    publishing

---
+++ scripts/testimonial.js
@@ -0,0 +1,14 @@
+//moving the testimonials//
+const wrapper = document.querySelector('.wrapper');
+const indicators = [...document.querySelectorAll('.indicators button')];
+
+let currentTestimonial = 0; // Def 0
+
+indicators.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        indicators[currentTestimonial].classList.remove('active');
+        wrapper.style.marginLeft = `-${100 * i}%`;
+        item.classList.add('active');
+        currentTestimonial = i;
+    })
+})
